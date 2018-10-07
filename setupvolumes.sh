df974694631de1078a9addf7a6243c0b8238cf71
naiktanmai <tanmai.d.naik@gmail.com>
2018-10-07 15:05:40 +0530

    FEAT(queries working)

--- setupvolumes.sh
+++
@@ -1,6 +0,0 @@
-docker volume create -d local-persist -o mountpoint=/home/tanmai/Documents/fuzzy-address-matching/mounts/data --name=mongodata
-
-
-docker volume create -d local-persist -o mountpoint=/home/tanmai/Documents/fuzzy-address-matching/mounts/esdata2 --name=esdata2
-
-docker volume create -d local-persist -o mountpoint=/home/tanmai/Documents/fuzzy-address-matching/connector --name=esconnector2
