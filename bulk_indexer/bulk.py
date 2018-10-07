8cc3db952c35c8b57eb44433a0db7a94968cd177
naiktanmai <tanmai.d.naik@gmail.com>
2018-10-07 15:05:40 +0530

    FEAT(queries working)

---
+++ bulk_indexer/bulk.py
@@ -0,0 +1,49 @@
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
+import csv,json
+import pprint
+def set_customers_mappings(es):
+    with open("settings.json") as settings_file:
+        settings = json.load(settings_file)
+        es.indices.create(index="customers", body=settings, ignore=400)
+
+def gen_data(records):
+    for record in records:
+        yield {
+            "_index": "customers",
+            "_type": "customer",
+            "_source": {
+                "Customer_Number": record["Customer_Number"],
+                "Service_street2": record["Service_street2"],
+                "Service_Street1": record["Service_Street1"],
+                "Service_City": record["Service_City"]
+            },
+        }
+
+
+def bulk_insert_customers(es, input_file):
+    data = json.loads(json.dumps(list(csv.DictReader(open(input_file)))))
+
+    for ok, result in streaming_bulk(
+            es,
+            gen_data(data),
+            index="customers",
+            doc_type='customer',
+            chunk_size=50
+        ):
+        action, result = result.popitem()
+        doc_id = '/%s/customer/%s' % ("customers", result['_id'])
+        if not ok:
+            print('Failed to %s document %s: %r' % ("bulk", doc_id, result))
+        else:
+            print(doc_id)
+
+if __name__ == '__main__':
+    es = Elasticsearch(
+        ['localhost'],
+        scheme="http",
+        port=9200,
+    )
+
+    set_customers_mappings(es)
+    bulk_insert_customers(es, input_file = "AWECRMDatasyn_067_176.csv")
